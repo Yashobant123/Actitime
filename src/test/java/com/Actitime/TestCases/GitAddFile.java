f8fdf221a8d18238ed0d955a9c4e5982594875bc
Yashobant123 <biswal.yb2369@gmail.com>
2023-07-27 15:11:31 +0530

    new file added

---
+++ src/test/java/com/Actitime/TestCases/GitAddFile.java
@@ -0,0 +1,14 @@
+package com.Actitime.TestCases;
+
+import org.testng.annotations.Test;
+
+public class GitAddFile {
+
+	@Test
+	public void addfile()
+	{
+		System.out.println("yoooooooooooo");
+
+	}
+
+}
